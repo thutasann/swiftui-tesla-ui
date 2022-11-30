1bc2eddece150989359eaa08cae047ae3699c6fd
thutasann <thutasann@gmail.com>
2022-11-30 08:19:18 +0630

    Initial Commit

---
+++ TeslaUI/TeslaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaUIApp.swift
+//  TeslaUI
+//
+//  Created by Thuta sann on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TeslaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
